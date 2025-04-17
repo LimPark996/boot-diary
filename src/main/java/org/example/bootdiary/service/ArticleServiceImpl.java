759b86f7486fcf8b3ff74db012642ed0449d9b7b
qus0in <qus0in@gmail.com>
2025-04-17 10:12:56 +0900

    클래스 구조

---
+++ src/main/java/org/example/bootdiary/service/ArticleServiceImpl.java
@@ -0,0 +1,8 @@
+package org.example.bootdiary.service;
+
+import org.springframework.stereotype.Service;
+
+// Component
+@Service // Spring
+public class ArticleServiceImpl implements ArticleService {
+}

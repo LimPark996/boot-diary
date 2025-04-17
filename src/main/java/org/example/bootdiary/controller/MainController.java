a3de9973abd7f9a840f18e1d1fb734dfc34ae1c6
qus0in <qus0in@gmail.com>
2025-04-17 10:35:09 +0900

    인덱스 페이지 완성

--- src/main/java/org/example/bootdiary/controller/MainController.java
+++ src/main/java/org/example/bootdiary/controller/MainController.java
@@ -1,7 +1,16 @@
 package org.example.bootdiary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("title", "블로그에 오신 걸 환영함당 🤗");
+        model.addAttribute("message", "저는 엄청난 개발자임당 🤪");
+        model.addAttribute("frontImage", "/assets/dino.jpeg");
+        return "index";
+    }
 }
